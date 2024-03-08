f93ecd9afd78506b0eb016aa2bc4bf4aac4cfafb
unknown <adrian40001@gmail.com>
2024-03-07 21:20:12 -0300

    adding desempregados.com original homepage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const path = require("path");
+
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+router.get("/desempregados", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/pages", "desempregados.html"));
 });
 
 router.get("/login", (req, res) => {
