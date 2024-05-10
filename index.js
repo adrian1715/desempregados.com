b6a4241dd6ec9c6a1242e308652785faa57cf5d6
unknown <adrian40001@gmail.com>
2024-05-09 21:38:30 -0300

    adding boilerplate layout

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const PORT = process.env.PORT || 3001;
+const ejsMate = require("ejs-mate");
 
 const getPages = require("./middlewares/getPages");
 const routes = require("./routes/index");
 
+// setting up views pages
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", "views/pages");
 app.use(express.static("public"));
@@ -15,6 +18,7 @@ app.use(getPages);
 
 // app.use("/api"); // restful api
 
+// setting up routes
 app.use("/", routes);
 
 app.listen(PORT, () => console.log("running on port", PORT));
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Homepage - desempregados.com" });
 });
 
 router.get("/desempregados", (req, res) => {
