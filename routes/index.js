3bf93b1449e8aaf7a6a14bb47d3bc396da9bac79
unknown <adrian40001@gmail.com>
2024-05-14 18:56:57 -0300

    adding desempregados homepage and fixing some styles

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Homepage - desempregados.com" });
 });
 
-router.get("/desempregados", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/pages", "desempregados.html"));
-});
+// router.get("/desempregados", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../views/pages", "desempregados.html"));
+// });
 
 router.get("/login", (req, res) => {
   res.render("login");
