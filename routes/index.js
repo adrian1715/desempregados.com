2c51d09f0a6c2093ef46f00ae1b70a824dcfd457
adrian1715 <adrian40001@gmail.com>
2024-05-14 23:28:36 -0300

    adding pages

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,22 @@ router.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../views/pages", "desempregados.html"));
 // });
 
+router.get("/vagas", (req, res) => {
+  res.render("vagas");
+});
+
+router.get("/cadastrar-vaga", (req, res) => {
+  res.render("cadastrar-vaga");
+});
+
+router.get("/sobre", (req, res) => {
+  res.render("sobre");
+});
+
+router.get("/contato", (req, res) => {
+  res.render("contato");
+});
+
 router.get("/login", (req, res) => {
   res.render("login");
 });
