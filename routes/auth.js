b109440534ebb7c2b1a053f1b0a55288fa4eb37a
Adrian <adrian40001@gmail.com>
2025-07-08 10:30:44 +0100

    cadastro page quick fix

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,12 @@ router.get("/login", (req, res) => {
   res.render("auth/login");
 });
 
-router.get("/cadastro", (req, res) => res.render("auth/cadastro"));
+router.get("/cadastro", (req, res) => {
+  const { user } = req.query; // gets user form the url params
+
+  res.render("auth/cadastro", {
+    user: user || null,
+  });
+});
 
 module.exports = router;
