afb066116c15e3d998752624076e0fc02478383e
Adrian <adrian40001@gmail.com>
2025-09-02 18:32:11 +0100

    auth backend update

--- models/User.js
+++ models/User.js
@@ -2,38 +2,37 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema(
+  {
+    // username: {
+    //   type: String,
+    //   required: true,
+    //   unique: true,
+    // },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, // regex validation
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["candidate", "company"],
+      required: true,
+    },
+    profile: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "role", // dyamically references a Candidate or Company
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    // match: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, // regex validation
-  },
-  // password: {
-  // type: String,
-  // required: true,
-  // },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  role: {
-    type: String,
-    enum: ["candidate", "company"],
-    // required: true,
-  },
-  profile: {
-    type: mongoose.Schema.Types.ObjectId,
-    // required: true,
-    refPath: "role",
-  },
-});
+  { timestamps: true }
+);
 
-userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" }); // tells passport to use email to login, instead of default username
 
 module.exports = mongoose.model("User", userSchema);
