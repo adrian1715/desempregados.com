339ab73d8480c350e29d3fe1a8934c0055a2c080
Adrian <adrian40001@gmail.com>
2025-10-27 22:22:25 +0000

    companies page update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const Company = require("../models/Company");
+const Candidate = require("../models/Candidate");
+
+const { truncateToLastWord } = require("../utils/string");
+
 // API
 router.use("/api", require("./api/index.js"));
 
@@ -20,8 +25,9 @@ router.get("/vagas", (req, res) => {
   res.render("vagas");
 });
 
-router.get("/empresas", (req, res) => {
-  res.render("empresas");
+router.get("/empresas", async (req, res) => {
+  const companies = await Company.find();
+  res.render("companies", { companies, truncateToLastWord });
 });
 
 router.use("/carreiras", require("./careers"));
